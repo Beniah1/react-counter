b1a489fae193ff87a313f967b75efce5dc36bcec
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2023-12-05 13:48:09 +0000

    Fix bug in login functionality

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ClassCounter from './ClassCounter';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ClassCounter />
+      </div>
+    );
+  }
+}
+
+export default App;
